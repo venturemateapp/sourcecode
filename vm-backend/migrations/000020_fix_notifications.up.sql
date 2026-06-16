c3ce2cccd30269936377c4e7cd61ee1fd47a6ec9
Ubuntu <ubuntu@ip-172-31-91-108.ec2.internal>
2026-06-16 00:13:05 +0000

    fix: add migrations to Docker image, fix 000020 migration for missing column

--- vm-backend/migrations/000020_fix_notifications.up.sql
+++ vm-backend/migrations/000020_fix_notifications.up.sql
@@ -1,3 +1,8 @@
-ALTER TABLE notifications RENAME COLUMN message TO description;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='notifications' AND column_name='message') THEN
+        ALTER TABLE notifications RENAME COLUMN message TO description;
+    END IF;
+END $$;
 ALTER TABLE notifications ADD COLUMN IF NOT EXISTS action_url TEXT;
 ALTER TABLE notifications ADD COLUMN IF NOT EXISTS action_label TEXT;
